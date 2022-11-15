101e431007e67381562b8402b434b877d96eca54
GaiaY <20302010064@fudan.edu.cn>
2022-11-15 17:58:47 +0800

    Drop files from .gitignore

--- .history/client/client_20221114134944.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <winsock2.h>
-#pragma comment(lib, "ws2_32.lib")
-
-//报错信息
-#define err(errMsg) printf("[error]%s failed! code: %d line: %d", errMsg, WSAGetLastError(), __LINE__);
-//端口信息
-#define PORT 8888
--- .history/server/server_20221114134220.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <winsock2.h>
-#pragma comment(lib, "ws2_32.lib") 
